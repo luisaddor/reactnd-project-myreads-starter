3c66a668689d43d0578665f4af80da504c1ce5d5
Luis Addor <luis.addor@mirumagency.com>
2018-08-02 18:43:17 -0300

    [...] Update to separate on shelfes by categorie.

--- src/MyReads.js
+++ src/MyReads.js
@@ -6,6 +6,18 @@ class MyReads extends Component {
     render () {
         let books = this.props.books;
 
+        let currentlyReading = books.filter(current => 
+            current.shelf === 'currentlyReading'
+        )
+
+        let want = books.filter(current => 
+            current.shelf === 'wantToRead'
+        )
+
+        let read = books.filter(current => 
+            current.shelf === 'read'
+        )
+
         return (
             <div className="list-books">
                 <div className="list-books-title">
@@ -17,7 +29,61 @@ class MyReads extends Component {
                             <h2 className="bookshelf-title">Currently Reading</h2>
                             <div className="bookshelf-books">
                                 <ol className="books-grid">
-                                    {books.map((book) => (
+                                    {currentlyReading.map((book) => (
+                                        <li key={book.id}>
+                                        <div className="book">
+                                            <div className="book-top">
+                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                                            <div className="book-shelf-changer">
+                                                <select>
+                                                <option value="move" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                                </select>
+                                            </div>
+                                            </div>
+                                            <div className="book-title">{ book.title }</div>
+                                            <div className="book-authors">{ book.authors }</div>
+                                        </div>
+                                        </li>
+                                    ))}
+                                </ol>
+                            </div>
+                        </div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Want to read</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {want.map((book) => (
+                                        <li key={book.id}>
+                                        <div className="book">
+                                            <div className="book-top">
+                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                                            <div className="book-shelf-changer">
+                                                <select>
+                                                <option value="move" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                                </select>
+                                            </div>
+                                            </div>
+                                            <div className="book-title">{ book.title }</div>
+                                            <div className="book-authors">{ book.authors }</div>
+                                        </div>
+                                        </li>
+                                    ))}
+                                </ol>
+                            </div>
+                        </div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Read</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {read.map((book) => (
                                         <li key={book.id}>
                                         <div className="book">
                                             <div className="book-top">
